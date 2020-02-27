336af97753211803761ef0b77b97580aebe11b62
AlexaEgorova <egorova.a007@gmail.com>
2020-02-28 01:04:34 +0300

    LabRab1's done

--- gradient/src/source.cpp
+++ gradient/src/source.cpp
@@ -3,63 +3,73 @@
 
 using namespace cv;
 
-void gradient(Mat& img, int step) {
-  for (int i=0; i + step < img.cols; i+=step) {
-
-    int color_step = 255 / img.cols;
-
+void PourWithGradient(Mat& img, int step)
+{
+  int color = 0;
+  for (int i = 0; i + step < img.cols; i += step)
+  {
     if (img.cols < 255) {
       img.col(i).setTo(i);
     }
-
     if (img.cols == 255) {
       for (int j = i; j < i + step; j++) {
         img.col(j).setTo(i);
       }
+      int remainder = img.cols % step;
+      for (int i = img.cols - remainder; i < img.cols; i++)
+        img.col(i).setTo(255);
     }
-
     if (img.cols > 255) {
-      img.col(i).setTo(i);
+      int recounted_step = img.cols / 255;
+      for (int j = i; j < i + step; j++) {
+        img.col(j).setTo(color);
+      }
     }
+    color++;
   }
-  int remainder = img.cols % step;
-  for (int i = img.cols - remainder; i < img.cols; i ++)
-    img.col(i).setTo(255);
+}
+
+void CorrectGamma(Mat& img, double gamma, double A=1.0) {
+  for (int i = 0; i < img.cols; i++) {
+    auto color = img.at<unsigned char>(0, i);
+    auto normColor = color / 255.0;
+    auto newColor = 255.0 * pow(normColor, gamma) * A;
+    img.col(i).setTo(newColor);
+  }
 }
 
 int main()
 {
-  Mat image(1500, 1500, CV_8U, Scalar(1));
-
-  int x1 = 0;
-  int y1 = 0;
-
-  int x2 = 0;
-  int y2 = 300;
+  Mat image(800, 800, CV_8U, Scalar(50));
 
   int wid1 = 255;
   int hei1 = 255;
 
-  int step = wid1 / 255;
+  int wid2 = 760;
+  int hei2 = 60;
 
-  cv::Rect roi(x1, y1, wid1, hei1);
-  cv::Mat roiMat = image(roi);
+  cv::Rect roiSoftGradient(0, 0, wid1, hei1);
+  cv::Mat matSoftGradient = image(roiSoftGradient);
 
-  cv::Rect roi10(x2, y2, wid1, hei1);
-  cv::Mat roi10Mat = image(roi10);
+  cv::Rect roiStepGradient(0, 300, wid1, hei1);
+  cv::Mat matStepGradient = image(roiStepGradient);
 
-  int cols1 = 255 * 2;
+  cv::Rect roiLongGradient(0, 600, wid2, hei2);
+  cv::Mat matLongGradient = image(roiLongGradient);
 
-  //for (int r = 0; r < cols1; r+=step) {
-  //  roiMat.col(r).setTo(r);
-  //  roi10Mat.col(r).setTo((r / 5) * 5);
-  //}
+  cv::Rect roiGamma(0, 705, wid2, hei2);
+  cv::Mat matGamma = image(roiGamma);
 
-  gradient(roiMat, 1);
-  gradient(roi10Mat, 20);
+  PourWithGradient(matSoftGradient, 1);
+  PourWithGradient(matStepGradient, 20);
+  PourWithGradient(matLongGradient, 3);
+  PourWithGradient(matGamma, 3);
 
-  namedWindow("Linear Gradient", CV_WINDOW_NORMAL);
-  imshow("Linear Gradient", image);
+  CorrectGamma(matGamma, 2);
+
+  namedWindow("Gradient", CV_WINDOW_NORMAL);
+  imshow("Gradient", image);
   waitKey(0);
 
 }
+
