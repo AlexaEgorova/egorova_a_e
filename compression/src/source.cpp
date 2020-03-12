21badf00376021ed87bc71ce8bae3f6e381ae4e4
AlexaEgorova <egorova.a007@gmail.com>
2020-03-13 01:36:26 +0300

    Lab2 finished

--- compression/src/source.cpp
+++ compression/src/source.cpp
@@ -24,36 +24,14 @@ string CompressImageToJpg(const Mat& src, const string& srcName, int compression
   return name;
 }
 
-Mat GetBlueChannel(const Mat& src, const Mat& channel) {
-
+void GetBlue(const Mat& src, Mat dst) {
   Mat empty_image = Mat::zeros(src.rows, src.cols, CV_8UC1);
   Mat result(src.rows, src.cols, CV_8UC3);
-  Mat in1[] = { channel, empty_image, empty_image };
-  int from_to1[] = { 0,0, 1,1, 2,2 };
-  mixChannels(in1, 3, &result, 1, from_to1, 3);
-  return result;
-}
-
-Mat GetGreenChannel(const Mat& src, const Mat& channel) {
-  Mat empty_image = Mat::zeros(src.rows, src.cols, CV_8UC1);
-  Mat result(src.rows, src.cols, CV_8UC3);
-  Mat in1[] = { empty_image, channel, empty_image };
-  int from_to1[] = { 0,0, 1,1, 2,2 };
-  mixChannels(in1, 3, &result, 1, from_to1, 3);
-  return result;
-}
-
-Mat GetRedChannel(const Mat& src, const Mat& channel) {
-  Mat empty_image = Mat::zeros(src.rows, src.cols, CV_8UC1);
-  Mat result(src.rows, src.cols, CV_8UC3);
-  Mat in1[] = { empty_image, empty_image, channel };
-  int from_to1[] = { 0,0, 1,1, 2,2 };
-  mixChannels(in1, 3, &result, 1, from_to1, 3);
-  return result;
+  Mat in1[] = { empty_image, empty_image, dst };
 }
 
 int main() {
-  Mat background(500, 1000, CV_8U, Scalar(0));
+  Mat background(700, 1300, CV_8UC3, Scalar(0));
 
   string path = "flowers.png";
   Mat img, img95, img65;
@@ -65,15 +43,52 @@ int main() {
   ReadSmallMat(name).copyTo(img65);
 
   vector<Mat> channels(3);
-  split(img, channels);
+  int rowsNumber = 3;
+  int columnsNumber = 3;
+  int height = img.size[0];
+  int width = img.size[1];
 
-  Mat result;
-  GetBlueChannel(img, channels[0]).copyTo(result);
-  imshow("CompressionBlue", result);
-  GetGreenChannel(img, channels[1]).copyTo(result);
-  imshow("CompressionGreen", result);
-  GetRedChannel(img, channels[2]).copyTo(result);
-  imshow("CompressionRed", result);
+  Mat matsPerRow[] = { img, img95, img65 };
+  for (int row = 0; row < rowsNumber; ++row) {
+    split(matsPerRow[row], channels);
+    Mat empty_image = Mat::zeros(channels[0].rows, channels[0].cols, CV_8UC1);
+    Mat mv[3] = { empty_image, empty_image, empty_image };
+
+    for (int column = 0; column < columnsNumber; ++column) {
+      Mat result;
+      mv[column] = channels[column];
+      merge(mv, 3, result);
+
+      if (row > 0) {
+        Mat prevMat = background(Rect(column*width, 0, width, height));
+        subtract(result, prevMat, result);
+      }
+
+      Mat matRoi = background(Rect(column*width, row*height, width, height));
+      result.copyTo(matRoi);
+
+      mv[column] = empty_image;
+    }
+  }
+
+  for (int row = 0; row < rowsNumber; ++row) {
+    Mat pic = matsPerRow[row];
+    Mat grayPic;
+    cvtColor(pic, grayPic, CV_RGB2GRAY);
+    Mat mv[3] = { grayPic, grayPic, grayPic };
+    merge(mv, 3, grayPic);
+
+
+    if (row > 0) {
+      Mat prevMat = background(Rect(columnsNumber*width, 0, width, height));
+      subtract(grayPic, prevMat, grayPic);
+    }
+
+    Mat matRoi = background(Rect(columnsNumber*width, row*height, width, height));
+    grayPic.copyTo(matRoi);
+  }
+
+  imshow("Compression&Channels", background);
 
   waitKey();
 }
